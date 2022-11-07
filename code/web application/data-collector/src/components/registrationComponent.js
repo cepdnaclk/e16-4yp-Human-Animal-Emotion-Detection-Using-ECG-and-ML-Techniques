38e37ef642a529d906ff2efb2871c97404faa88d
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2022-11-07 19:37:36 +0530

    pass name to next screen

--- code/web application/data-collector/src/components/registrationComponent.js
+++ code/web application/data-collector/src/components/registrationComponent.js
@@ -1,55 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from 'react-router-dom';
 
 
 
 export default function Register(){
     let navigate = useNavigate();
-    function handleSubmit(values) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [age, setAge] = useState('');
+    const [gender, setGender] = useState('');
+
+    const handleSubmit = event =>{
         let path = `/videos`;
-        navigate(path);
+        navigate(path, {
+            state: {name: firstName,}
+        });
+        setFirstName('');
+        setLastName('');
+        setAge('');
+        setGender('');
     }
 
     return(
         <div>
-            <div className="Auth-form-container">
-                <form className="Auth-form" onSubmit={(values) => handleSubmit(values)}>
-                    <div className="Auth-form-content">
-                        <h3 className="Auth-form-title">Subject Registration Form </h3>
-                        <div className="form-group mt-3">
+            <div >
+                <form onSubmit={ handleSubmit}>
+                    <div >
+                        <h3 >Subject Registration Form </h3>
+                        <div >
                             <label>First Name</label>
                             <input
                                 type="text"
-                                className="form-control mt-1"
                                 placeholder="first name"
+                                id="firstname"
+                                value={firstName}
+                                onChange={event => setFirstName(event.target.value)}
                             />
                         </div>
-                        <div className="form-group mt-3">
+                        <div >
                             <label>Last Name</label>
                             <input
                                 type="text"
-                                className="form-control mt-1"
                                 placeholder="last name"
+                                id="lastname"
                             />
                         </div>
                         <div className="form-group mt-3">
                             <label>Age</label>
                             <input
                                 type="number"
-                                className="form-control mt-1"
                                 placeholder="age in years"
+                                id="age"
                             />
                         </div>
-                        <div className="form-group mt-3">
+                        <div >
                             <label>Gender</label>
-                            <select >
+                            <select id="gender" >
                                 <option value="female">Female</option>
                                 <option value="male">Male</option>
                             </select>
                         </div>
-                        <div className="d-grid gap-2 mt-3">
-                            <button type="submit" className="btn btn-primary">
-                                Submit
+                        <div >
+                            <button type="submit" >
+                                Register
                             </button>
                         </div>
                     </div>
