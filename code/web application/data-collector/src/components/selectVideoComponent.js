55bcee3d3f9d2630fcb22d4570c1db460f26db59
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2022-11-07 19:38:12 +0530

    add links to videos

--- code/web application/data-collector/src/components/selectVideoComponent.js
+++ code/web application/data-collector/src/components/selectVideoComponent.js
@@ -1,9 +1,38 @@
-import { React } from "react";
+import React  from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
-export default function SelectVideo() {
+
+export default function SelectVideo({navigation}) {
+    const location = useLocation();
+    let navigate = useNavigate();
+    let name = location.state? location.state.name : " ";
     return (
         <div>
-            List of Videos
+        <div>
+           Subject Name: {name.toUpperCase()}
+        </div>
+        <div>
+            Subject ID: XXX
+        </div>
+            <div>
+                <ul>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/1")} >Video 01</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/2")} >Video 02</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/3")} >Video 03</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/4")} >Video 04</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/5")} >Video 05</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/6")} >Video 06</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/7")} >Video 07</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/8")} >Video 08</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/9")} >Video 09</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/10")} >Video 10</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/11")} >Video 11</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/12")} >Video 12</Button></li>
+                    <li ><Button variant="contained" onClick={() => navigate("/play/13")} >Video 13</Button></li>
+
+                </ul>
+            </div>
         </div>
     );
 }
